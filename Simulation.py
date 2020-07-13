47d5a77421e86e4f06cf50631c8da34efba7f485
ramyelgendi <57822979+ramyelgendi@users.noreply.github.com>
2020-07-13 17:36:45 +0200

    Created main file

---
+++ Simulation.py
@@ -0,0 +1,43 @@
+import FileParsing
+import sys
+import AStar
+import visual
+inputFile = input("Enter address for your input file. [Format: /Users/ramyelgendi/Desktop/AStarRouter/input.txt] :\n")
+# Parsing Input
+file = FileParsing.FileParsing(inputFile)
+if not file.Exists():
+    print("Parsing file does not exist!")
+    sys.exit()
+
+netList = file.Parse()
+
+# Create Grid
+router = AStar.AStar(1, 1000, 1000)
+
+pathsList = []
+
+for line in netList:
+    Path = []
+    for i, node in enumerate(line):
+        try:
+            source = node
+            target = line[i + 1]
+            netPath, G, F = router.Path(int(source[0]), int(source[1]), int(source[2]), int(target[0]), int(target[1]),
+                                        int(target[2]))
+            Path = Path + netPath
+        except IndexError:
+            pass
+    pathsList.append(Path)
+
+# Writing Output
+outputFile = inputFile.replace('.txt', '_out.txt')
+status = file.OutputList(pathsList, outputFile)
+if status:
+    print("Completed Successfully! ")
+    visual = visual.visual(pathsList)
+    visual.figure() # Draw
+    sys.exit()
+
+else:
+    print("Failed! ")
+    sys.exit()
